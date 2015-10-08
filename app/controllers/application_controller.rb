da34a826a2a7d0fd0db748796bdb6a47bc060cf2
Dian Pan <dian.di.pan@gmail.com>
2015-10-08 16:57:53 -0400

    add logged in checker helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,15 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    else
+      nil
+    end
+  end
+
+  def logged_in?
+    session[:user_id]
   end
 
 end
