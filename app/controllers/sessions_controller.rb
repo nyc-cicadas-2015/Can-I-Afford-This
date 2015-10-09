34a1611c7973f47f613a8ee2269886cde2d9a9e4
Dian Pan <dian.di.pan@gmail.com>
2015-10-08 21:14:00 -0400

    add session create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,18 @@
 class SessionsController < ApplicationController
 
   def new
+    user = User.new
+  end
+
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Unable to find password & username combination. Please try again."
+      redirect_to login_path
+    end
   end
 
 end
