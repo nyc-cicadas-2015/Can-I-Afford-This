7e98fe8fe208e4a6c41fec82bb727fc99905cf4f
Dian Pan <dian.di.pan@gmail.com>
2015-10-08 21:57:18 -0400

    add destroy method code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,12 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.delete(:user_id)
+    flash[:message] = "You've been successfully logged out!"
+    redirect_to root_path
+  end
+
   private
   def session_params
     params.require(:session).permit(:email, :password)
