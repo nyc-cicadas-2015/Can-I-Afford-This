b127e4048128571dcc2868c13bdc24a95666bb43
Dian Pan <dian.di.pan@gmail.com>
2015-10-08 21:42:53 -0400

    clean up controller spec code

--- spec/controllers/sessions_controller.rb
+++ spec/controllers/sessions_controller.rb
@@ -25,34 +25,7 @@ describe SessionsController do
     end
   end
 
-  describe "POST #create" do
-    describe "When Successful" do
-      let(:user_params) { { user: attributes_for(:user) } }
 
-      it "Creates a user" do
-        post(:create, user_params)
-        expect(response).to redirect_to root_path
-      end
-
-      it "Increased number of users in the database by 1" do
-        expect{post(:create, user_params)}.to change{User.count}.by(1)
-      end
-    end
-
-    describe "When Unsuccessful" do
-      it "redirects to login" do
-        post(:create, user: {name: nil, email: nil, password: nil })
-        expect(response).to redirect_to login_path
-      end
-
-      it "sets a flash error" do
-        post(:create, user: {name: nil, email: nil, password: nil })
-        expect(flash[:alert]).to have_content "Unable to find password & username combination"
-      end
-
-    end
-
-  end
 
 
 
