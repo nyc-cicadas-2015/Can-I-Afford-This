2be865927234b7f7a028cdd4c0adbb1c4205beb6
Dian Pan <dian.di.pan@gmail.com>
2015-10-09 10:56:23 -0400

    test that a new user can see the sign up form

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -6,4 +6,10 @@ describe 'create a new user' do
     visit root_path
     expect(page).to have_content('sign up')
   end
+
+  it 'should have a sign up form' do
+    visit root_path
+    click_link 'sign up'
+    expect(current_path).to eq new_user_path
+  end
 end
