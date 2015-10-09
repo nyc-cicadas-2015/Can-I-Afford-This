f4668a4ecea2bf60b9a7aa77d901eb019307a72e
Dian Pan <dian.di.pan@gmail.com>
2015-10-09 10:54:19 -0400

    test that a user can see the signup link

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -1,1 +1,9 @@
 require 'rails_helper'
+
+describe 'create a new user' do
+  let(:user_attr) { attributes_for(:user) }
+  it 'should have a signup link' do
+    visit root_path
+    expect(page).to have_content('sign up')
+  end
+end
