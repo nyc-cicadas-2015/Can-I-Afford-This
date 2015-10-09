dffb1c701b8bacc33ad9f8a9af5a196627952af6
Dian Pan <dian.di.pan@gmail.com>
2015-10-08 22:46:19 -0400

    edit session delete route path

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy', :via => :delete
+  delete '/logout' => 'sessions#destroy'
 
 
 
