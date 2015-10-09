9de6589be159ce5efe5dd883328565d74271be57
Dian Pan <dian.di.pan@gmail.com>
2015-10-08 21:11:04 -0400

    add new spec method

--- spec/controllers/sessions_controller.rb
+++ spec/controllers/sessions_controller.rb
@@ -1,1 +1,13 @@
+require 'rails_helper'
 
+describe SessionsController do
+  describe "GET #new" do
+    it "Creates a new session" do
+      get :new
+      expect(assigns(:user)).to be_a(User)
+    end
+  end
+
+
+
+end
