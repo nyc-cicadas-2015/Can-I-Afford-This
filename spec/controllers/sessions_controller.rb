e07f868697f0c939bcfd0c229de2132e8e01b67e
Dian Pan <dian.di.pan@gmail.com>
2015-10-08 21:17:13 -0400

    add post create controller test

--- spec/controllers/sessions_controller.rb
+++ spec/controllers/sessions_controller.rb
@@ -18,6 +18,17 @@ describe SessionsController do
     end
   end
 
+  describe "POST #create" do
+    describe "When Successful" do
+      let(:user_params) { { user: attributes_for(:user) } }
+
+      it "Creates a user" do
+        post(:create, user_params)
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
 
 
 end
