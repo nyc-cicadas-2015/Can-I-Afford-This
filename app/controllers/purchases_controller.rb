e3f547fbb89253f91731fcab578f148d79a0ab0b
julrusak <juliannarusak@gmail.com>
2015-10-10 13:21:49 -0400

    Update purchase create error message

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -31,7 +31,7 @@ class PurchasesController < ApplicationController
     if @purchase.update_attributes purchases_params
       redirect_to user_path(session[:user_id])
     else
-      flash[:error] = "Your purchase must be greater than $0."
+      flash[:error] = "Your purchase must have a name and be greater than $0."
       redirect_to user_path(session[:user_id])
     end
   end
