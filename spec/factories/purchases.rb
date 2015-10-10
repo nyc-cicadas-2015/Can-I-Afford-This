4ca16ae4545a603bb62f89207a3d8c30fb31d8dc
julrusak <juliannarusak@gmail.com>
2015-10-10 13:47:50 -0400

    Update FactoryGirl to add purchase title. Passing all tests

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     association :user, factory: :user
     association :purchase_type, factory: :purchase_type
     cost { Faker::Commerce.price }
+    title { Faker::Company.buzzword }
   end
 end
