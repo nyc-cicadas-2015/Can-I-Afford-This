429aa52efdf9fa402ae86c4a57890248250e3bf0
Dian Pan <dian.di.pan@gmail.com>
2015-10-09 20:46:53 -0400

    test that a signed in user can add a new saving from their profile page

--- spec/features/savings_spec.rb
+++ spec/features/savings_spec.rb
@@ -1,2 +1,18 @@
 require 'rails_helper'
 
+describe "Savings" do
+  let(:login_page) { LoginPageHelper.new }
+  let(:user) { attributes_for(:user)}
+  before :each do
+    create(:user)
+    login_page.visit_page.login(user)
+  end
+
+  describe 'User can add a new saving' do
+    it 'by visiting their profile page' do
+      visit profile_path(session[:user_id])
+      expect(page).to have_content("Add savings")
+    end
+
+  end
+end
