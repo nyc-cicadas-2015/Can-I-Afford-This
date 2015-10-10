3e5c952ea2560d25230610a817bd62f1334112e4
Dian Pan <dian.di.pan@gmail.com>
2015-10-09 20:56:49 -0400

    test that a user can successfullyy add to their savings

--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -7,7 +7,7 @@ class SavingsController < ApplicationController
   def create
     @user = User.find(session[:user_id])
     saving = @user.savings.build(saving_params)
-    saving.save ? flash[:message] = "$#{saving.amount} added!" : flash[:error] = "Amount cannot be $0"
+    saving.save ? flash[:message] = "Nice job! $#{saving.amount} added!" : flash[:error] = "Amount cannot be $0"
     redirect_to profile_path(session[:user_id])
   end
 
--- spec/features/savings_spec.rb
+++ spec/features/savings_spec.rb
@@ -8,10 +8,18 @@ describe "Savings" do
     login_page.visit_page.login(user)
   end
 
-  describe 'User can add a new saving' do
+  describe 'User can add a new saving amount' do
     it 'by visiting their profile page' do
       visit profile_path(@user.id)
       expect(page).to have_content("Add savings")
+    end
+
+    it 'with valid attributes' do
+      visit profile_path(@user.id)
+      click_link "Add savings"
+      fill_in "new_saving", :with => 200
+      click_button "Add savings"
+      expect(page).to have_content(200)
     end
 
   end
