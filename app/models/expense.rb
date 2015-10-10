ebe16c1deed75ac1d4c08455b04c7a0cdd0b3877
julrusak <juliannarusak@gmail.com>
2015-10-09 22:05:02 -0400

    Add has_one relationships

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,5 @@ class Expense < ActiveRecord::Base
   validates_numericality_of :amount, :greater_than => 0
 
   belongs_to :user
+  has_one :expense_type
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,5 @@ class Purchase < ActiveRecord::Base
   validates_presence_of :cost
 
   belongs_to :user
+  has_one :purchase_type
 end
