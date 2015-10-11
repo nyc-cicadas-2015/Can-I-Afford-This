bff81215116bcc55696df8e86cf2f760e97a9ab0
Dian Pan <dian.di.pan@gmail.com>
2015-10-11 17:02:57 -0400

    add back expenses spec

--- spec/features/expenses_spec.rb
+++ spec/features/expenses_spec.rb
@@ -9,5 +9,23 @@ describe "Expenses" do
   end
 
   describe 'User can create new expense' do
+    it 'by visiting the expense page' do
+      visit new_expense_path
+      expect(page).to have_content("Enter your expenses")
+    end
+
+    it 'with valid attributes' do
+      visit new_expense_path
+      fill_in "expense_amount", :with => 200
+      click_button "Add Expense"
+      expect(page).to have_content(200)
+    end
+
+    it 'with valid attributes' do
+      visit new_expense_path
+      fill_in "expense_amount", :with => nil
+      click_button "Add Expense"
+      expect(page).to have_content("Your expense must be greater than $0.")
+    end
   end
 end
