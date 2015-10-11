99e3f7715353d30b2edbc4bed5d3a9cb66f0918a
Dian Pan <dian.di.pan@gmail.com>
2015-10-11 17:56:49 -0400

    add expensetype method amount range to categorize the purchase price

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
   end
 
   def create
--- app/models/expense_type.rb
+++ app/models/expense_type.rb
@@ -2,4 +2,18 @@ class ExpenseType < ActiveRecord::Base
   validates_presence_of :name
 
   belongs_to :expense
+
+  def amount_range(price)
+    if price(0..1000)
+      ExpenseType.name = "small"
+    elsif price(1001..2000)
+      ExpenseType.name = "medium"
+    elsif price >= 3001
+      ExpenseType.name = "large"
+    else
+      nil
+    end
+  end
+
+
 end
