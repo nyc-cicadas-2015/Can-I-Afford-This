a5ea34968356611b97799e71bc40ae48afb62670
Dian Pan <dian.di.pan@gmail.com>
2015-10-11 17:05:44 -0400

    add an add expense link to user profile

--- spec/features/user_expense_vs_income_spec.rb
+++ spec/features/user_expense_vs_income_spec.rb
@@ -9,23 +9,5 @@ describe "User Profile page" do
   end
 
   describe 'User can compare how their expense vs income stack up' do
-    it 'shows user their total monthly expenses' do
-      visit user_path(session[:user_id])
-      expect(page).to have_content("Enter your expenses")
-    end
-
-    it 'with valid attributes' do
-      visit new_expense_path
-      fill_in "expense_amount", :with => 200
-      click_button "Add Expense"
-      expect(page).to have_content(200)
-    end
-
-    it 'with valid attributes' do
-      visit new_expense_path
-      fill_in "expense_amount", :with => nil
-      click_button "Add Expense"
-      expect(page).to have_content("Your expense must be greater than $0.")
-    end
   end
 end
