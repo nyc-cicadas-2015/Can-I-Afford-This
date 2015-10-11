3f5b2d2825601844e3bf91fe820858ee2968f17d
Dian Pan <dian.di.pan@gmail.com>
2015-10-11 13:54:50 -0400

    add ability to total all purchase amounts

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,7 +3,10 @@ class Purchase < ActiveRecord::Base
   validates_presence_of :title
   validates_numericality_of :cost, :greater_than => 0
 
-
   belongs_to :user
   has_one :purchase_type
+
+  def self.total_purchase_amount
+    pluck(:cost).reduce(:+) || 0
+  end
 end
