38aeb017ebcd6c7b2ae02813bdfbb89ecc4e9d76
Dian Pan <dian.di.pan@gmail.com>
2015-10-11 19:26:28 -0400

    add income to expense diff calculator

--- app/models/expense_type.rb
+++ app/models/expense_type.rb
@@ -4,16 +4,23 @@ class ExpenseType < ActiveRecord::Base
   belongs_to :expense
 
   def amount_range(price)
+    payoff_time = @payoff_time || 0
     if price(0..1000)
       ExpenseType.name = "small"
+      payoff_time = 6
     elsif price(1001..2000)
       ExpenseType.name = "medium"
+      payoff_time = 12
     elsif price >= 3001
       ExpenseType.name = "large"
+      payoff_time = 60
     else
       nil
     end
   end
 
+  def income_to_expense_diff(income, expense)
+    income - expense
+  end
 
 end
