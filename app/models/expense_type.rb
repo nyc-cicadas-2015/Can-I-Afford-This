b91d18a489db92380700969aabfab4772bf72c1e
Dian Pan <dian.di.pan@gmail.com>
2015-10-11 22:23:42 -0400

    move amount range method from expense to purchase model

--- app/models/expense_type.rb
+++ app/models/expense_type.rb
@@ -2,20 +2,4 @@ class ExpenseType < ActiveRecord::Base
   validates_presence_of :name
 
   belongs_to :expense
-
-  def amount_range(price)
-    payoff_time = @payoff_time || 0
-    if price(0..1000)
-      ExpenseType.name = "small"
-      payoff_time = 6
-    elsif price(1001..2000)
-      ExpenseType.name = "medium"
-      payoff_time = 12
-    elsif price >= 3001
-      ExpenseType.name = "large"
-      payoff_time = 60
-    else
-      nil
-    end
-  end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -9,4 +9,20 @@ class Purchase < ActiveRecord::Base
   def self.total_purchase_amount
     pluck(:cost).reduce(:+) || 0
   end
+
+  def amount_range(price)
+    if price(0..1000)
+      ExpenseType.name = "small"
+      payoff_time = 6
+    elsif price(1001..2000)
+      ExpenseType.name = "medium"
+      payoff_time = 12
+    elsif price >= 3001
+      ExpenseType.name = "large"
+      payoff_time = 60
+    else
+      nil
+    end
+  end
+
 end
