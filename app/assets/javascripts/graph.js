cb52bb45754f91519508bc0eac69bc0f1ee2dc3d
julrusak <juliannarusak@gmail.com>
2015-10-11 21:49:49 -0400

    Render json objects for data

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -4,6 +4,7 @@ $.ajax({
   url: 'data',
   dataType: 'json',
   success: function (data) {
+    debugger
     draw(data);
   },
   error: function (result) {
@@ -13,37 +14,37 @@ $.ajax({
 
 function draw(data) {
   var color = d3.scale.category20b();
-  var width = 420,
-  barHeight = 20;
+  var height = 420,
+  barWidth = 20;
 
-  var x = d3.scale.linear()
-  .range([0, width])
+  var y = d3.scale.linear()
+  .range([0, height])
   .domain([0, d3.max(data)]);
 
   var chart = d3.select("#graph")
-  .attr("width", width)
-  .attr("height", barHeight * data.length);
+  .attr("height", height)
+  .attr("width", barWidth * data.length);
 
   var bar = chart.selectAll("g")
   .data(data)
   .enter().append("g")
   .attr("transform", function (d, i) {
-  return "translate(0," + i * barHeight + ")";
+  return "translate(0," + i * barWidth + ")";
 });
 
 bar.append("rect")
-  .attr("width", x)
-  .attr("height", barHeight - 1)
+  .attr("widthL", y)
+  .attr("width", barWidth - 1)
   .style("fill", function (d) {
   return color(d)
 })
 
 bar.append("text")
-  .attr("x", function (d) {
-  return x(d) - 10;
+  .attr("y", function (d) {
+  return y(d) - 10;
   })
-  .attr("y", barHeight / 2)
-  .attr("dy", ".35em")
+  .attr("x", barWidth / 2)
+  .attr("dx", ".35em")
   .style("fill", "white")
   .text(function (d) {
   return d;
