0aab3bb3d39cfa012f803ff1c1f5862617e7e173
Dian Pan <dian.di.pan@gmail.com>
2015-10-12 12:20:12 -0400

    remove purchase type model method code

--- app/models/purchase_type.rb
+++ app/models/purchase_type.rb
@@ -1,14 +1,4 @@
 class PurchaseType < ActiveRecord::Base
   validates_presence_of :name
   belongs_to :purchase
-
-  def payoff_time
-    if PurchaseType.name == 'small'
-      payoff_time = 6
-    elsif PurchaseType.name == 'medium'
-      payoff_time = 12
-    elsif PurchaseType.name == 'large'
-      payoff_time = 60
-    end
-  end
 end
