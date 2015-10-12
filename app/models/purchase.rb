032e5dd37e9c287dd4bf2d1372d5e1c3b04ae006
Dian Pan <dian.di.pan@gmail.com>
2015-10-12 14:39:56 -0400

    clean up copy and code

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -40,7 +40,7 @@ class Purchase < ActiveRecord::Base
     max_payoff_time = self.add_payoff_time
     months_to_payoff = (purchase_price / income_to_expense_diff.to_f).ceil
     if months_to_payoff <= max_payoff_time
-      "Yes you can afford it!  By saving smart and not spending more than you earn, it will take you #{months_to_payoff} months to buy your Now start saving!"
+      "Yes you can afford it!  By saving smart and not spending more than you earn, it will take you #{months_to_payoff} months to buy your #{self.title}. \n Now start saving!"
     else
       "Sorry! No, you cannot afford it. Our conscenous would not be clear if we put you down this path.  It will take you #{months_to_payoff} months to save up for this purchase.  Consider lowering your existing expenses before attempting to make this purchase. Don't worry, we'll show you how."
     end
