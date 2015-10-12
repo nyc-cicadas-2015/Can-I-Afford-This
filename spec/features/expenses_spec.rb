fb430cd3872d38cf01dec2ec501d7576306ba363
julrusak <juliannarusak@gmail.com>
2015-10-12 15:13:01 -0400

    Fix redirect after single expense create

--- spec/features/expenses_spec.rb
+++ spec/features/expenses_spec.rb
@@ -11,20 +11,20 @@ describe "Expenses" do
   describe 'User can create new expense' do
     it 'by visiting the expense page' do
       visit new_expense_path
-      expect(page).to have_content("Enter your expenses")
+      expect(page).to have_content("Add an expense")
     end
 
     it 'with valid attributes' do
       visit new_expense_path
-      fill_in "__Housing", :with => 200
-      click_button "Add Expenses"
+      fill_in "expense_amount", :with => 200
+      click_button "Add Expense"
       expect(page).to have_content(200)
     end
 
     it 'with valid attributes' do
       visit new_expense_path
-      fill_in "__Housing", :with => nil
-      click_button "Add Expenses"
+      fill_in "expense_amount", :with => nil
+      click_button "Add Expense"
       expect(page).to have_content("Your expense must be greater than $0.")
     end
   end
