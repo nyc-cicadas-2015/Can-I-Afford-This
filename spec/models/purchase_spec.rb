825c08482a1ddb7c1670777e00cd1e023bb4e58d
Dian Pan <dian.di.pan@gmail.com>
2015-10-12 15:21:08 -0400

    edit purchase model spec

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 describe Purchase do
-  it 'is valid with a title, cost and purchase type id'
-  it 'is invalid without a title'
-  it 'is invalid without a cost'
-  it 'is invalid without a purchase type id'
-  it 'returns the total purchase amount'
-  it 'returns the purchase type id'
-  it 'returns the payoff time'
-  it 'returns whether or not the user can afford this purchase'
+  it 'is valid with a title, cost and purchase type id' do
+    purchase = Purchase.new(
+      title: "beats headphones",
+      cost: 250,
+      purchase_type_id: 1)
+    expect(purchase).to be_valid
+  end
+  it 'is invalid without a title' do
+    purchase = Purchase.new(title: nil)
+    purchase.valid?
+    expect(purchase.errors[:title]).to include("can't be blank")
+  end
+  it 'is invalid without a cost' do
+    purchase = Purchase.new(cost: nil)
+    purchase.valid?
+    expect(purchase.errors[:cost]).to include("can't be blank")
+  end
+  it 'is invalid without a purchase type id' do
+    purchase = Purchase.new(purchase_type_id: nil)
+    expect(purchase.valid?).to eq(false)
+  end
 end
