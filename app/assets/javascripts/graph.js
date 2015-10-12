28a6e2bac8dc588c9a3837e69facd395eb53549f
julrusak <juliannarusak@gmail.com>
2015-10-11 23:18:13 -0400

    Bar charts dynamic, not representing data

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,27 +1,27 @@
-  $.ajax({
-    type: "GET",
-    contentType: "application/json; charset=utf-8",
-    url: 'data',
-    dataType: 'json',
-    success: function (data) {
-      run(data);
-    },
-    error: function (result) {
-      console.error("You have an error");
-    }
-  });
+$.ajax({
+  type: "GET",
+  contentType: "application/json; charset=utf-8",
+  url: 'data',
+  dataType: 'json',
+  success: function (data) {
+    run(data);
+  },
+  error: function (result) {
+    console.error("You have an error");
+  }
+});
 
-function run(data){
+function run(d){
+  console.log(d)
   var svg = d3.select("svg");
 
   svg.selectAll("rect")
-    .data(data)
+    .data(d)
     .transition()
     .duration(1500)
-      .attr("x", function(d,i) { return i*90+50; })
+      .attr("x", function(d,i) { return i*90+10; })
       .attr("y", 0)
       .attr("width", 20)
       .attr("height", function(d,i) { return d; })
       .style("fill", "steelblue");
-
 }
