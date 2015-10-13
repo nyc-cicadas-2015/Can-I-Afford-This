8501cb4f4486febbdb964705681cb7547d3f53e4
Dian Pan <dian.di.pan@gmail.com>
2015-10-13 16:20:31 -0400

    add total savings method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,11 @@ class User < ActiveRecord::Base
   has_many :savings
 
   def total_expenses
-    expenses.sum(:amount)
+    expenses.sum(:amount) ||= 0
+  end
+
+  def total_savings
+    savings.sum(:amount) ||= 0
   end
 
 end
