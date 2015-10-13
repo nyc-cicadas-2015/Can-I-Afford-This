4e747dca4b82c03f7f10730fb800396565e9c3b4
Dian Pan <dian.di.pan@gmail.com>
2015-10-13 10:09:30 -0400

    update savings feature spec

--- spec/features/savings_spec.rb
+++ spec/features/savings_spec.rb
@@ -6,16 +6,17 @@ describe "Savings" do
   before :each do
     @user = create(:user)
     login_page.visit_page.login(user)
+    click_link "Planned Purchases"
   end
 
   describe 'User can add a new saving amount' do
-    it 'by visiting their profile page' do
-      visit profile_path(@user.id)
+    it 'by visiting their planned purchases page' do
+      visit userpurchases_path(@user.id)
       expect(page).to have_content("Saved")
     end
 
     it 'saves with valid attributes' do
-      visit profile_path(@user.id)
+      visit userpurchases_path(@user.id)
       click_link "Add savings"
       fill_in "saving_amount", :with => 200
       click_button "Add savings"
@@ -23,7 +24,7 @@ describe "Savings" do
     end
 
     it 'does not save with invalid attributes' do
-      visit profile_path(@user.id)
+      visit userpurchases_path(@user.id)
       click_link "Add savings"
       fill_in "saving_amount", :with => nil
       click_button "Add savings"
