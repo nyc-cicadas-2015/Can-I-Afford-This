15bbe551ed8cfd97ac2c96d528755a4326b0ed80
julrusak <juliannarusak@gmail.com>
2015-10-13 18:05:34 -0400

    Add ajax call

--- app/assets/javascripts/spending_averages.js
+++ app/assets/javascripts/spending_averages.js
@@ -1,60 +1,72 @@
-$(function () {
-    $('#spending-averages').highcharts({
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Your Spending Percentages'
-        },
-        subtitle: {
-            text: 'Compared to National Averages'
-        },
-        xAxis: {
-            categories: ['Housing', 'Transportation', 'Food', 'Phone', 'Misc', 'Savings'],
-            title: {
-                text: 'Categories'
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Percentage',
-                align: 'high'
+$(document).ready(function(){
+    $.ajax({
+        url: '/graph/_net_savings_data',
+        method: "get"
+    }).done(function(data){
+        makeChart(data);
+    }).fail(function(e){
+        console.log("Failed:" + e);
+    })
+
+
+    $(function () {
+        $('#spending-averages').highcharts({
+            chart: {
+                type: 'bar'
             },
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        tooltip: {
-            valueSuffix: ' millions'
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    enabled: true
+            title: {
+                text: 'Your Spending Percentages'
+            },
+            subtitle: {
+                text: 'Compared to National Averages'
+            },
+            xAxis: {
+                categories: ['Housing', 'Transportation', 'Food', 'Phone', 'Misc', 'Savings'],
+                title: {
+                    text: 'Categories'
                 }
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            x: -40,
-            y: 80,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-            shadow: true
-        },
-        credits: {
-            enabled: false
-        },
-        series: [{
-            name: 'Your spending',
-            data: [27, 31, 35, 20, 5]
-        }, {
-            name: 'Average Spending',
-            data: [35, 15, 7, 3, 30, 10]
-        }]
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Percentage',
+                    align: 'high'
+                },
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            tooltip: {
+                valueSuffix: ' millions'
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -40,
+                y: 80,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+                shadow: true
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'Your spending',
+                data: [27, 31, 35, 20, 5]
+            }, {
+                name: 'Average Spending',
+                data: [35, 15, 7, 3, 30, 10]
+            }]
+        });
     });
-});
+})
