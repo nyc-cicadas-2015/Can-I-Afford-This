3c94d9a96451a98f6f187860b1e9ae05b061e0e7
julrusak <juliannarusak@gmail.com>
2015-10-13 16:37:55 -0400

    Find a working highcharts model

--- app/assets/javascripts/spending_averages.js
+++ app/assets/javascripts/spending_averages.js
@@ -1,25 +1,60 @@
 $(function () {
-    $('#container').highcharts({
-        data: {
-            table: 'datatable'
-        },
+    $('#spending-averages').highcharts({
         chart: {
-            type: 'column'
+            type: 'bar'
         },
         title: {
-            text: 'Data extracted from a HTML table in the page'
+            text: 'Historic World Population by Region'
+        },
+        subtitle: {
+            text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
         },
         yAxis: {
-            allowDecimals: false,
+            categories: ['Housing', 'Transportation', 'Food', 'Phone', 'Misc'],
             title: {
-                text: 'Units'
+                text: null
+            }
+        },
+        xAxis: {
+            min: 0,
+            title: {
+                text: 'Population (millions)',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
             }
         },
         tooltip: {
-            formatter: function () {
-                return '<b>' + this.series.name + '</b><br/>' +
-                    this.point.y + ' ' + this.point.name.toLowerCase();
+            valueSuffix: ' millions'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
             }
-        }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 80,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Your spending',
+            data: [107, 31, 635, 203, 2]
+        }, {
+            name: 'Average Spending',
+            data: [133, 156, 947, 408, 6]
+        }]
     });
 });
