98ffdbf93892393e6ee7bbcc7490cca04b3f66c9
Dian Pan <dian.di.pan@gmail.com>
2015-10-13 16:08:45 -0400

    update code order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,14 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
+  def logged_in?
+    !!session[:user_id]
+  end
+
   def current_user
     if logged_in?
       @user ||= User.find(session[:user_id])
     end
-  end
-
-  def logged_in?
-    !!session[:user_id]
   end
 
 end
