b74c714f8eb93c58cc9f1642cce36dc520ab6dc7
Dian Pan <dian.di.pan@gmail.com>
2015-10-14 15:47:35 -0400

    update welcome flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      flash[:message] = "Success!"
+      flash[:message] = "Welcome #{user.name}!"
       redirect_to root_path
     else
       flash[:error] = "email/password already exist, please login"
