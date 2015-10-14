51b13b00bca1cc898869fa31190c6f255dcc3c0c
Dian Pan <dian.di.pan@gmail.com>
2015-10-14 14:47:32 -0400

    add include statements to database calls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(expenses: [:expense_type]).find(params[:id])
   end
 
   def create
