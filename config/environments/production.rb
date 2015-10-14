28441a570de87cf76c20abff1279553af3c57e47
Dian Pan <dian.di.pan@gmail.com>
2015-10-14 18:49:14 -0400

    remove code in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,6 @@ Rails.application.configure do
   config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.assets.precompile += ['rollover.js', 'highcharts.js']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
