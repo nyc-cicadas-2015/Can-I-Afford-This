50eba00e0226af760a709b31e479e6703df60c21
Dian Pan <dian.di.pan@gmail.com>
2015-10-14 15:49:47 -0400

    update redirect path for new user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     if user.save
       session[:user_id] = user.id
       flash[:message] = "Welcome #{user.name}!"
-      redirect_to root_path
+      redirect_to profile_path current_user
     else
       flash[:error] = "email/password already exist, please login"
       redirect_to '/login'
