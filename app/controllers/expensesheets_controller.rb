e9e3bcb6c794fd7cf04783e231c37579132e3015
julrusak <juliannarusak@gmail.com>
2015-10-14 10:20:02 -0400

    Initial edit form

--- app/controllers/expensesheets_controller.rb
+++ app/controllers/expensesheets_controller.rb
@@ -1,5 +1,5 @@
 class ExpensesheetsController < ApplicationController
-    before_action :find_user, only: [:new, :create, :show, :update]
+    before_action :find_user, only: [:new, :create, :show, :edit, :update]
 
   def new
     @expenses = Expense.new
@@ -18,11 +18,11 @@ class ExpensesheetsController < ApplicationController
   end
 
   def edit
-    @expenses = @user.expenses
+    @expenses = @user.expenses.includes(:expense_type).each_with_object({}) { |e, obj| obj[e.expense_type.name] = e }
   end
 
   def update
-    Expense.expense_sheet(params[:expense][:amount]).each do |e|
+    Expense.expense_sheet(params[:amount]).each do |e|
       @user.expenses << e
       flash[:error] = "Oops! Someting went wrong." unless e.save
     end
