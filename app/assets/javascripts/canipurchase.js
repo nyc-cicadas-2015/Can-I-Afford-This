b9dc7d72286c7be0679dcb807fdda4fd4fde7f3c
Dian Pan <dian.di.pan@gmail.com>
2015-10-14 20:31:03 -0400

    Add js file for ajax call to canipurchase page

---
+++ app/assets/javascripts/canipurchase.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#canipurchase').on('click', function(e){
+        e.preventDefault();
+        $.ajax({
+            debugger;
+            url: '',
+            method: 'GET'
+        }).done(function(data){
+            $('').html(data);
+            $('').show();
+        }).fail(function(){
+            console.error("Error, something went wrong.");
+        });
+    });
+});
