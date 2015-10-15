6f9e9e906e49009f47dd857972a66b54a126dabc
Dian Pan <dian.di.pan@gmail.com>
2015-10-15 11:20:53 -0400

    edit ajax so that each div appends the correct data

--- app/assets/javascripts/canipurchase.js
+++ app/assets/javascripts/canipurchase.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-    $('#canipurchase').on('click', function(e){
+    $('.canipurchase').on('click', function(e){
         e.preventDefault();
+        //remove global variable in refactor
+        this_div_id = $(this).attr("href").substring(11)
         var url = $(e.target).attr('href')
         $.ajax({
             url: url,
             method: 'GET'
         }).done(function(data){
-            $('#caniafford-results').html(data);
-            $('#result-copy').show();
+          // debugger
+            $('#caniafford-results-' + this_div_id).append(data);
         }).fail(function(){
             console.error("Error, something went wrong.");
         });
